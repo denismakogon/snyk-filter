40b0c522b3737d1f97df373d44f60cee8cd9fef5
Denis Makogon <denys.makogon@oracle.com>
2019-04-17 19:38:15 +0300

    Snyk filter: 0.0.5 release [skip ci]

--- config/version.go
+++ config/version.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Version of Snyk-filter CLI
-var Version = "0.0.4"
+var Version = "0.0.5"
 
 func GetVersion(versionType string) string {
 	base := "https://github.com/denismakogon/snyk-filter/releases"
