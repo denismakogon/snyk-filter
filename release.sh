e594b146d42b6b8b57ca6aa3e85cbd34abd7fb27
Denis Makogon <denys.makogon@oracle.com>
2019-04-17 14:23:21 +0300

    remove release from CI

--- release.sh
+++ release.sh
@@ -25,13 +25,5 @@ tag="$version"
 git add -u
 git commit -m "Snyk filter: $version release [skip ci]"
 git tag -f -a $tag -m "version $version"
-git push -q https://${GH_TOKEN}@github.com/denismakogon/snyk-filter
-git push -q https://${GH_TOKEN}@github.com/denismakogon/snyk-filter origin $tag
-
-# For GitHub
-url='https://api.github.com/repos/denismakogon/snyk-filter/releases'
-output=$(curl -s -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY -d "{\"tag_name\": \"$version\", \"name\": \"$version\"}" $url)
-upload_url=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["upload_url"]' | sed -E "s/\{.*//")
-html_url=$(echo "$output" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["html_url"]')
-curl --data-binary "@snyk-filter_linux"  -H "Content-Type: application/octet-stream" -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY $upload_url\?name\=snyk-filter_linux >/dev/null
-curl --data-binary "@snyk-filter_mac"    -H "Content-Type: application/octet-stream" -u $GH_DEPLOY_USER:$GH_DEPLOY_KEY $upload_url\?name\=snyk-filter_mac >/dev/null
+git push
+git push -q origin $tag
